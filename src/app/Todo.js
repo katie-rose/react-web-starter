1595aff647afe1e30f0b1c0b0562b8ca79df1b00
Katie Rose <katiefelten@gmail.com>
2019-09-06 00:44:32 -0400

    :zap: basic JSX practice

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,9 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export class Todo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nums: [1, 2, 3, 4]
+    };
+  }
+  handleClick(e) {
+    const nums = [...this.state.nums, 6];
+
+    this.setState({ nums });
+  }
   render() {
     return (
-      <div>hello</div>
+      <div>
+        <button onClick={this.handleClick.bind(this)}>click</button>
+        {this.state.nums.map(num => (
+          <h3 key={num}>{num}</h3>
+        ))}
+      </div>
     );
   }
 }
